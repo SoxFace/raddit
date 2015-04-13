906e94f6f8cec2990a051ce0d49d312049eef666
soxface <sonyacorcoran@gmail.com>
2015-04-13 21:41:24 +1000

    all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  
+  resources :comments
+
   devise_for :users
-  root to: "links#index"
-  resources :links
+  
+  resources :links do
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+    resources :comments
+  end
+  root "links#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
